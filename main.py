01d811cfbff52be4a1bd526dca6f21094b7eb17f
Jacob Bashista <tgb.bashista@gmail.com>
2018-10-08 11:27:13 -0400

    Updated Main for Readability

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-import grovepi
+import grovepi as gpi
 import time
 from grove_i2c_barometic_sensor_BMP180 import BMP085
 from gps import GPS
 from lcd import LCD
 
-TEMP_SENSOR = 0 # Port A0
-SOUND_SENSOR = 2 # Port A2
-BMP_SENSOR = BMP085(0x77, 1) # Register pressure sensor Port I2C-1
-GPS_SENSOR = GPS('/dev/ttyAMA0', 4800) # Connect Serial to GPS Device
-LCD_DISPLAY = LCD()
+tempSensor = 0 # Port A0 is for the Temperature Sensor
+soundSensor = 2 # Port A2 is for the Sound Sensor
+bmpSensor = BMP085(0x77, 1) # Register pressure sensor Port I2C-1
+gpsSensor = GPS('/dev/ttyAMA0', 4800) # Connect Serial to GPS Device
+lcdDisplay = LCD() # Create a reference to the LCD Display
 
-LCD_DISPLAY.setRGB(0,0, 255)
-LCD_DISPLAY.setText("Hello!")
+lcdDisplay.setRGB(0,0, 255) # Set Background Color of Display
+lcdDisplay.setText("Hello!")
 time.sleep(2)
-LCD_DISPLAY.setText("Making sure everything is ready!")
-time.sleep(3)
-LCD_DISPLAY.setText("Ready to Go!")
-time.sleep(1)
+lcdDisplay.setText("Prepping Sensors!")
+time.sleep(2)
+lcdDisplay.setText("Ready to Go!")
+time.sleep(2)
+
 while True:
     try:
-        LCD_DISPLAY.setText("Gathering Sensor Data")
-        curTemp = grovepi.temp(TEMP_SENSOR) # Get temperature in celcius
-        curSound = grovepi.analogRead(SOUND_SENSOR) # Get current sound level in ???
-        curPressure = BMP_SENSOR.readPressure()/100 # Get current presure in millibar
-        curAltitude = BMP_SENSOR.readAltitude()  # Get the current altitude in meters
-        curCoords = GPS_SENSOR.read() # Get the current data from the GPS module
+        lcdDisplay.setText("Gathering Sensor Data")
+        curTemp = gpi.temp(TEMP_SENSOR) # Get temperature in celcius
+        curSound = gpi.analogRead(SOUND_SENSOR) # Get current sound level in ???
+        curPressure = bmpSensor.readPressure()/100 # Get current presure in millibar
+        curAltitude = bmpSensor.readAltitude()  # Get the current altitude in meters
+        curCoords = gpsSensor.read() # Get the current data from the GPS module
 
         print str(curTemp) + ', ' + str(curSound) + ', ' + str(curPressure) + ', ' + str(curAltitude) # Print Current Readings from Sensors
         print str(curCoords.latitude) + ', ' + str(curCoords.longitude) + ', ' + str(curCoords.altitude) # Print Current Readings from GPS
-        LCD_DISPLAY.setText("Got Data")
+        lcdDisplay.setText("Got Data")
 
     except:
         print("Error")
-        LCD_DISPLAY.setText("Error")
+        lcdDisplay.setText("Error")
 
     time.sleep(1) # Wait 1 Second Until next loop
